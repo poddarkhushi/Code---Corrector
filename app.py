9fb6adc987b6388337ee2338616cffc53960773d
Khushi Poddar <poddar.khushi1214@gmail.com>
2026-01-09 22:12:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def correct_code_openai(code, lang):
         "model": "gpt-4o-mini",   
         "messages": [
             {"role": "system", "content": f"You are a code correction assistant. The user is writing in {lang}."},
-            {"role": "user", "content": f"Fix this {lang} code. You will receive code. Fix it and give a short explaination for  all the changes that you have made in the form of comments. Return ONLY the corrected code. Do NOT include markdown or ```python``` blocks.  Output ONLY raw corrected code and nothing else.\n\n{code}"}
+            {"role": "user", "content": f"Fix this {lang} code and explain ONLY the corrections (do NOT rewrite entire code unnecessarily):\n\n{code}"}
         ]
     }
 
